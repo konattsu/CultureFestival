8cdfd880161c4e802bd7f8a1c7d64eaf9c07ae79
konattsu <139730998+konattsu@users.noreply.github.com>
2025-09-05 20:24:48 +0000

    使ってないゴミ削除

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,6 @@ import ErrorBoundary from "./components/ErrorBoundary";
 import LoadingSpinner from "./components/LoadingSpinner.tsx";
 import ScrollToTop from "./components/ScrollToTop.tsx";
 import { AuthProvider } from "./context/AuthContext.tsx";
-import { SettingsProvider } from "./context/SettingsContext.tsx";
-import { ThemeProvider } from "./context/ThemeContext.tsx";
 import MainLayout from "./layouts/MainLayout.tsx";
 
 const Home = lazy(() => import("./pages/Home.tsx"));
@@ -24,28 +22,24 @@ const ContentsRoutes = lazy(() => import("./pages/ContentsRoutes.tsx"));
 const App: React.FC = () => {
   return (
     <ErrorBoundary>
-      <ThemeProvider>
-        <AuthProvider>
-          <SettingsProvider>
-            <ScrollToTop />
-            <Suspense fallback={<LoadingSpinner />}>
-              <Routes>
-                <Route path="/" element={<MainLayout />}>
-                  <Route index element={<Home />} />
-                  <Route path="contents/*" element={<ContentsRoutes />} />
-                  <Route path="magazine" element={<Magazine />} />
-                  <Route path="bulletin-board" element={<BulletinBoard />} />
-                  <Route path="cms" element={<CMSSelection />} />
-                  <Route path="bulletin-cms" element={<BulletinCMS />} />
-                  <Route path="admin-login" element={<AdminLogin />} />
-                  <Route path="map" element={<Map />} />
-                  <Route path="*" element={<NotFound />} />
-                </Route>
-              </Routes>
-            </Suspense>
-          </SettingsProvider>
-        </AuthProvider>
-      </ThemeProvider>
+      <AuthProvider>
+        <ScrollToTop />
+        <Suspense fallback={<LoadingSpinner />}>
+          <Routes>
+            <Route path="/" element={<MainLayout />}>
+              <Route index element={<Home />} />
+              <Route path="contents/*" element={<ContentsRoutes />} />
+              <Route path="magazine" element={<Magazine />} />
+              <Route path="bulletin-board" element={<BulletinBoard />} />
+              <Route path="cms" element={<CMSSelection />} />
+              <Route path="bulletin-cms" element={<BulletinCMS />} />
+              <Route path="admin-login" element={<AdminLogin />} />
+              <Route path="map" element={<Map />} />
+              <Route path="*" element={<NotFound />} />
+            </Route>
+          </Routes>
+        </Suspense>
+      </AuthProvider>
     </ErrorBoundary>
   );
 };
